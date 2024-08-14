38b7ddd3f8d9eb2220d0c4b892a070e35c039204
Tamara Zoric <zoric.m.tamara@gmail.com>
2024-08-14 12:49:52 +0200

    feat: Add PageHero reusable component
    Introduce a new PageHero component to the theme, including slots for title and lead content. The component also comes with responsive styling and color transitions for a polished visual presentation.

---
+++ .vitepress/theme/components/PageHero.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="PageHero">
+    <h1 class="title">
+      <slot name="title" />
+    </h1>
+    <p class="lead">
+      <slot name="lead" />
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.PageHero {
+  padding: 48px 24px;
+  text-align: center;
+  margin: 0 auto;
+  max-width: 688px;
+}
+
+@media (min-width: 768px) {
+  .PageHero {
+    padding: 64px 32px;
+  }
+}
+
+.title,
+.lead,
+.PageHero :deep(.link) {
+  transition: color 0.25s;
+}
+
+.title {
+  line-height: 32px;
+  font-size: 32px;
+  font-weight: 500;
+  margin-bottom: 0.3em;
+}
+
+@media (min-width: 768px) {
+  .title {
+    line-height: 40px;
+    font-size: 40px;
+  }
+}
+
+.lead {
+  padding-top: 8px;
+  font-size: 16px;
+  font-weight: 500;
+  color: var(--vt-c-text-2);
+}
+
+.lead a {
+  color: var(--vt-c-brand);
+}
+
+.PageHero :deep(.link) {
+  color: var(--vt-c-brand);
+}
+
+.PageHero :deep(.link:hover) {
+  color: var(--vt-c-brand-dark);
+}
+</style>
