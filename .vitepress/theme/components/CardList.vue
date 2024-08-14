bfaa226e809091937630f621309dab44fbb901a2
Tamara Zoric <zoric.m.tamara@gmail.com>
2024-08-14 12:48:26 +0200

    feat: Add CardList reusable component with shuffling and filtering support
    Introduces a new CardList component in Vue, supporting item shuffling and filtering based on provided props. This component ensures that items are correctly displayed and includes conditional rendering for an optional link to browse all items.

---
+++ .vitepress/theme/components/CardList.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, shallowRef } from 'vue'
+
+const props = defineProps<{
+  items: Array<any>;
+  filter?: (item: any) => boolean | undefined;
+  cardComponent: any;
+  showLinkToAll?: boolean;
+  shuffleItems?: boolean;
+  browseLinkText?: string;
+  browseLinkUrl?: string;
+}>()
+
+const mounted = ref(false)
+const items = shallowRef(props.items)
+
+const filtered = computed(() => {
+  return props.filter ? items.value.filter(props.filter) : items.value
+})
+
+onMounted(() => {
+  mounted.value = true
+  const platinum = items.value.filter((p) => p.platinum)
+  const normal = items.value.filter((p) => !p.platinum)
+
+  if (props.shuffleItems) {
+    shuffle(platinum)
+    shuffle(normal)
+  }
+
+  items.value = [...platinum, ...normal]
+})
+
+function shuffle(array: Array<any>) {
+  let currentIndex = array.length
+  let temporaryValue
+  let randomIndex
+
+  // while there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    // and swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
+
+</script>
+
+<template>
+  <div v-show="mounted" class="CardList">
+    <!-- to skip SSG since the partners are shuffled -->
+    <ClientOnly>
+      <component
+        :is="cardComponent"
+        v-for="item in filtered"
+        :key="item.id || item.name"
+        :data="item"
+      />
+    </ClientOnly>
+
+    <a
+      class="browse-all"
+      :href="props.browseLinkUrl"
+      v-if="showLinkToAll && filtered.length % 2"
+    >
+      {{ browseLinkText }}
+    </a>
+  </div>
+</template>
+
+
+<style scoped>
+.CardList {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.browse-all {
+  color: var(--vt-c-text-2);
+  transition: color 0.5s ease;
+  font-size: 1.2em;
+  text-align: center;
+  padding-top: 240px;
+  display: block;
+  border-radius: 4px;
+  border: 1px solid var(--vt-c-divider-light);
+  width: 48.5%;
+  margin-bottom: 36px;
+}
+
+.browse-all:hover {
+  color: var(--vt-c-text-1);
+}
+
+@media (max-width: 768px) {
+  .browse-all {
+    display: none;
+  }
+}
+</style>
