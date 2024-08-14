1e3be27b37f64c7e571e2ed0f0d7b5ae0138301e
Tamara Zoric <zoric.m.tamara@gmail.com>
2024-08-14 13:16:44 +0200

    feat: Add new Talent page hero and join sections
    This commit introduces two new components: TalentHero and TalentJoin. TalentHero showcases certified freelancers and includes links for more information. TalentJoin provides a call-to-action for developers to apply for long-term positions.

---
+++ src/talents/components/TalentHero.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import PageHero from '@theme/components/PageHero.vue'
+import partnerConfig from '../partnerConfig.js'
+
+defineProps<{ title?: string }>()
+</script>
+
+<template>
+  <PageHero>
+    <template #title>{{ title || partnerConfig.pageHeroBanner.title }}</template>
+    <template #lead>
+      Vue Talent are certified individual freelancers. Payments, compliance and vetting are administrated by our partner
+      <a class="link" :href="partnerConfig.companyWebsiteLink" target="_blank">{{ partnerConfig.partnerName }}</a>.
+      Interested to join the list?
+      <a class="link" :href="partnerConfig.applyPage" target="_blank">Apply here</a>.
+    </template>
+  </PageHero>
+</template>
---
+++ src/talents/components/TalentJoin.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CallToActionSection from '@theme/components/CallToActionSection.vue'
+import partnerConfig from '../partnerConfig.js'
+</script>
+
+<template>
+  <CallToActionSection
+    v-if="partnerConfig.applyPage"
+    title="Become a listed developer"
+    description="Get a long-term part-time or full-time position at company looking for a Vue.js developer."
+    :link="partnerConfig.applyPage"
+    linkText="Apply to join"
+  />
+</template>
